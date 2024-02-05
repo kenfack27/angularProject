be5977c65a8bb31fc083c01e5d146e46bcaf253f
kenfack27 <88100916+kenfack27@users.noreply.github.com>
2024-02-05 16:07:47 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'',component:AuthComponent, title:'home page'},
+    { path:'home',component:HomePageComponent, title:'home'},
+];
