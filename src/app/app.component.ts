7b11bc2c747d435564cb6a5892d23fa193997d8f
kenfack27 <88100916+kenfack27@users.noreply.github.com>
2024-02-05 16:12:38 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AuthComponent } from './pages/auth/auth.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule,NavbarComponent,FooterComponent,HomePageComponent,AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
